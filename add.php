f1d5414814d474dcd4dfb39ee9a245d4c1048bc2
unknown <net119@net100.itec.noc.open.ed.jp>
2014-05-29 15:44:59 +0900

    データベース接続処理追加

--- add.php
+++ add.php
@@ -22,7 +22,27 @@
     	echo '著者名が未入力です。';
     	exit;
     }
-    
+
+	// データベース接続情報設定
+	$dsn = 'mysql:dbname=books_admin;host=localhost;charset=utf8';
+
+	$db_username = 'root';
+	$db_password = '';
+
+	// データベース接続
+	$conn = new PDO($dsn, $db_username, $db_password);
+
+	if(!$conn)
+	{
+    	echo '接続失敗';
+	}
+	else
+	{
+    	echo '接続成功';
+	}
+
+
+
 
     ?>
 
