b3c35c50fab78165f3ce0f9fe3c49079e4c65fa7
unknown <net119@net100.itec.noc.open.ed.jp>
2014-05-29 15:42:41 +0900

    図書データ登録処理入力チェック仮追加

--- add.php
+++ add.php
@@ -4,10 +4,25 @@
     </head>
 
     <body>
-    	<h3>登録処理</h3>
+    	<h3>図書データ登録 登録処理</h3>
     <?php
 
     print_r($_POST);
+
+    // 図書名のチェック
+    if(empty($_POST['book_name']))
+    {
+    	echo '図書名が未入力です。';
+    	exit;
+    }
+
+    // 著者名のチェック
+    if(empty($_POST['author_name']))
+    {
+    	echo '著者名が未入力です。';
+    	exit;
+    }
+    
 
     ?>
 
