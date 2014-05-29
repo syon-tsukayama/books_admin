f06163331233690d366eb25e38b867507004c739
unknown <net119@net100.itec.noc.open.ed.jp>
2014-05-29 15:22:06 +0900

    図書データ新規登録処理追加

---
+++ add.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+
+    <body>
+    	<h3>登録処理</h3>
+    <?php
+
+    print_r($_POST);
+
+    ?>
+
+
+    </body>
+
+</html>
---
+++ add_form.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+
+    <body>
+	<h3>登録処理入力画面</h3>
+	
+    <form action="add.php" method="post">
+    	図書名：<input type="text" name="book_name" /><br />	
+    	図書名カナ：<input type="text" name="book_kana" /><br />
+    	著者名：<input type="text" name="author_name" /><br />	
+    	著者名カナ：<input type="text" name="author_kana" /><br />
+
+    	<input type="submit" value="登録" />
+    </form>
+
+    </body>
+</html>
