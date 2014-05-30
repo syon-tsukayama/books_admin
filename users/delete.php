658a412ff4ea5b33ee5116f5c15019ae41aa1b70
syon-tsukayama <tsukayama@syon.co.jp>
2014-05-30 14:16:46 +0900

    図書削除処理をベースに利用者削除処理作成

--- users/delete.php
+++ users/delete.php
@@ -7,18 +7,18 @@ output_html_header();
 ?>
 
     <body>
-        <h3>図書データ削除 削除処理</h3>
+        <h3>利用者データ削除 削除処理</h3>
     <?php
 
-    // 図書IDチェック
-    if(empty($_GET['book_id']))
+    // 利用者IDチェック
+    if(empty($_GET['user_id']))
     {
-        echo '図書が指定されていません';
+        echo '利用者が指定されていません';
         exit;
     }
 
     // 入力データの半角空白削除
-    $book_id = trim($_GET['book_id']);
+    $user_id = trim($_GET['user_id']);
 
     // データベース接続
     $conn = connect_database();
@@ -30,14 +30,14 @@ output_html_header();
 
     // 新規登録SQL作成
     $sql =<<<EOS
-DELETE FROM `books` WHERE `id` = :book_id
+DELETE FROM `users` WHERE `id` = :user_id
 EOS;
 
     // SQL実行準備
     $stmt = $conn->prepare($sql);
 
     // 登録するデータを設定
-    $stmt->bindValue(':book_id', $book_id);
+    $stmt->bindValue(':user_id', $user_id);
 
     // SQL実行
     $result = $stmt->execute();
@@ -52,7 +52,7 @@ EOS;
     }
     ?>
 
-        <a href="index.php" class="btn btn-default">図書データ一覧</a>
+        <a href="index.php" class="btn btn-default">利用者データ一覧</a>
 
         <?php
         // フッタ出力
