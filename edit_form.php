d52964ce22272377a89ec7b81e9e001a044bfb37
syon-tsukayama <tsukayama@syon.co.jp>
2014-05-30 11:25:34 +0900

    図書データ編集フォームbootstrapによる装飾追加

--- edit_form.php
+++ edit_form.php
@@ -1,10 +1,12 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
     </head>
 
     <body>
-		<h3>図書データ更新 編集画面</h3>
+        <div class="container">
+    		<h3>図書データ更新 編集画面</h3>
 
         <?php
 
@@ -30,10 +32,7 @@
         if(!$conn)
         {
             echo '接続失敗';
-        }
-        else
-        {
-            echo '接続成功';
+            exit;
         }
 
         // 検索SQL作成
@@ -60,16 +59,50 @@ EOS;
         }
         ?>
 
-    <form action="edit.php" method="post">
-        <input type="hidden" name="book_id" value="<?php echo $row['id']; ?>">
-    	図書名：<input type="text" name="book_name" value="<?php echo $row['book_name']; ?>" /><br />	
-    	図書名カナ：<input type="text" name="book_kana" value="<?php echo $row['book_kana']; ?>" /><br />
-    	著者名：<input type="text" name="author_name" value="<?php echo $row['author_name']; ?>" /><br />	
-    	著者名カナ：<input type="text" name="author_kana" value="<?php echo $row['author_kana']; ?>" /><br />
+            <form action="edit.php" method="post" class="form-horizontal" role="form">
+                <input type="hidden" name="book_id" value="<?php echo $row['id']; ?>" />
 
-    	<input type="submit" value="更新" />
-    	<a href="index.php">図書データ一覧</a>
-    </form>
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">図書名</label>
+                    <div class="col-xs-4">
+                        <input type="text" name="book_name" class="form-control" value="<?php echo $row['book_name']; ?>" />
+                    </div>
+                </div>
+                
 
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">図書名カナ</label>
+                    <div class="col-xs-4">
+                        <input type="text" name="book_kana" class="form-control" value="<?php echo $row['book_kana']; ?>" />
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">著者名</label>
+                    <div class="col-xs-4">
+                        <input type="text" name="author_name" class="form-control" value="<?php echo $row['author_name']; ?>" />
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">著者名カナ</label>
+                    <div class="col-xs-4">
+                        <input type="text" name="author_kana" class="form-control" value="<?php echo $row['author_kana']; ?>" />
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                        <input type="submit" value="更新" class="btn btn-primary" />
+
+                        <a href="index.php" class="btn btn-default">図書データ一覧</a>
+                    </div>
+                </div>
+
+            </form>
+        </div>
+
+        <script src="js/jquery-1.11.1.js"></script>
+        <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
