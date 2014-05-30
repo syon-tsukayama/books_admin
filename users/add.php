16a58f848710bfe75bdbf194c77d6a5ffeba3817
syon-tsukayama <tsukayama@syon.co.jp>
2014-05-30 14:03:17 +0900

    図書登録処理をベースに利用者登録処理作成

--- users/add.php
+++ users/add.php
@@ -7,28 +7,21 @@ output_html_header();
 ?>
 
     <body>
-        <h3>図書データ登録 登録処理</h3>
+        <h3>利用者データ登録 登録処理</h3>
     <?php
 
     // 図書名のチェック
-    if(empty($_POST['book_name']))
+    if(empty($_POST['name']))
     {
-        echo '図書名が未入力です。';
-        exit;
-    }
-
-    // 著者名のチェック
-    if(empty($_POST['author_name']))
-    {
-        echo '著者名が未入力です。';
+        echo '氏名が未入力です。';
         exit;
     }
 
     // 入力データの半角空白削除
-    $book_name = trim($_POST['book_name']);
-    $book_kana = trim($_POST['book_kana']);
-    $author_name = trim($_POST['author_name']);
-    $author_kana = trim($_POST['author_kana']);
+    $name = trim($_POST['name']);
+    $kana = trim($_POST['kana']);
+    $gender = trim($_POST['gender']);
+    $tel = trim($_POST['tel']);
 
     // データベース接続
     $conn = connect_database();
@@ -40,19 +33,19 @@ output_html_header();
 
     // 新規登録SQL作成
     $sql =<<<EOS
-INSERT INTO  `books`
-(`book_name`, `book_kana`, `author_name`, `author_kana`, `created`, `updated`)
-VALUES (:book_name, :book_kana, :author_name, :author_kana, NOW(), NOW())
+INSERT INTO `users`
+(`name`, `kana`, `gender`, `tel`, `created`, `updated`)
+VALUES (:name, :kana, :gender, :tel, NOW(), NOW())
 EOS;
 
     // SQL実行準備
     $stmt = $conn->prepare($sql);
 
     // 登録するデータを設定
-    $stmt->bindValue(':book_name', $book_name);
-    $stmt->bindValue(':book_kana', $book_kana);
-    $stmt->bindValue(':author_name', $author_name);
-    $stmt->bindValue(':author_kana', $author_kana);
+    $stmt->bindValue(':name', $name);
+    $stmt->bindValue(':kana', $kana);
+    $stmt->bindValue(':gender', $gender);
+    $stmt->bindValue(':tel', $tel);
 
     // SQL実行
     $result = $stmt->execute();
@@ -67,7 +60,7 @@ EOS;
     }
     ?>
 
-        <a href="index.php" class="btn btn-default">図書データ一覧</a>
+        <a href="index.php" class="btn btn-default">利用者データ一覧</a>
 
         <?php
         // フッタ出力
