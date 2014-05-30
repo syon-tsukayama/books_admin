f00161498bd54d2712fcb4cb6d42407c4a387504
syon-tsukayama <tsukayama@syon.co.jp>
2014-05-30 10:56:57 +0900

    新規登録フォームbootstrapによる装飾追加

--- add_form.php
+++ add_form.php
@@ -1,20 +1,51 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
     </head>
 
     <body>
 		<h3>図書データ登録 入力画面</h3>
 
-    <form action="add.php" method="post">
-    	図書名：<input type="text" name="book_name" /><br />	
-    	図書名カナ：<input type="text" name="book_kana" /><br />
-    	著者名：<input type="text" name="author_name" /><br />	
-    	著者名カナ：<input type="text" name="author_kana" /><br />
+        <form action="add.php" method="post" class="form-horizontal" role="form">
+            <div class="form-group">
+                <label class="col-sm-2 control-label">図書名</label>
+                <div class="col-xs-4">
+                    <input type="text" name="book_name" class="form-control" />
+                </div>
+            </div>
 
-    	<input type="submit" value="登録" />
-    	<a href="index.php">図書データ一覧</a>
-    </form>
+            <div class="form-group">
+                <label class="col-sm-2 control-label">図書名カナ</label>
+                <div class="col-xs-4">
+                    <input type="text" name="book_kana" class="form-control" />
+                </div>
+            </div>
 
+            <div class="form-group">
+                <label class="col-sm-2 control-label">著者名</label>
+                <div class="col-xs-4">
+                    <input type="text" name="author_name" class="form-control " />
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="col-sm-2 control-label">著者名カナ</label>
+                <div class="col-xs-4">
+                    <input type="text" name="author_kana" class="form-control" />
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                    <input type="submit" value="登録" class="btn btn-primary" />
+
+                    <a href="index.php" class="btn btn-default">図書データ一覧</a>
+                </div>
+            </div>
+        </form>
+
+        <script src="js/jquery-1.11.1.js"></script>
+        <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
