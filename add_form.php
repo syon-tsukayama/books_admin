f8aff751e8ddd95c0371135c6a9b318b411e1e81
syon-tsukayama <tsukayama@syon.co.jp>
2014-05-30 11:05:39 +0900

    インデント修正

--- add_form.php
+++ add_form.php
@@ -5,46 +5,48 @@
     </head>
 
     <body>
-		<h3>図書データ登録 入力画面</h3>
+        <div class="container">
+            <h3>図書データ登録 入力画面</h3>
 
-        <form action="add.php" method="post" class="form-horizontal" role="form">
-            <div class="form-group">
-                <label class="col-sm-2 control-label">図書名</label>
-                <div class="col-xs-4">
-                    <input type="text" name="book_name" class="form-control" />
+            <form action="add.php" method="post" class="form-horizontal" role="form">
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">図書名</label>
+                    <div class="col-xs-4">
+                        <input type="text" name="book_name" class="form-control" />
+                    </div>
                 </div>
-            </div>
 
-            <div class="form-group">
-                <label class="col-sm-2 control-label">図書名カナ</label>
-                <div class="col-xs-4">
-                    <input type="text" name="book_kana" class="form-control" />
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">図書名カナ</label>
+                    <div class="col-xs-4">
+                        <input type="text" name="book_kana" class="form-control" />
+                    </div>
                 </div>
-            </div>
 
-            <div class="form-group">
-                <label class="col-sm-2 control-label">著者名</label>
-                <div class="col-xs-4">
-                    <input type="text" name="author_name" class="form-control " />
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">著者名</label>
+                    <div class="col-xs-4">
+                        <input type="text" name="author_name" class="form-control " />
+                    </div>
                 </div>
-            </div>
 
-            <div class="form-group">
-                <label class="col-sm-2 control-label">著者名カナ</label>
-                <div class="col-xs-4">
-                    <input type="text" name="author_kana" class="form-control" />
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">著者名カナ</label>
+                    <div class="col-xs-4">
+                        <input type="text" name="author_kana" class="form-control" />
+                    </div>
                 </div>
-            </div>
 
-            <div class="form-group">
-                <div class="col-sm-offset-2 col-sm-10">
-                    <input type="submit" value="登録" class="btn btn-primary" />
+                <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                        <input type="submit" value="登録" class="btn btn-primary" />
 
-                    <a href="index.php" class="btn btn-default">図書データ一覧</a>
+                        <a href="index.php" class="btn btn-default">図書データ一覧</a>
+                    </div>
                 </div>
-            </div>
-        </form>
-
+            </form>
+        </div>
+        
         <script src="js/jquery-1.11.1.js"></script>
         <script src="js/bootstrap.min.js"></script>
     </body>
