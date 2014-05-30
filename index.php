d2f74f477ec0c110aff3c73cbfb9b6757646e551
syon-tsukayama <tsukayama@syon.co.jp>
2014-05-30 10:27:38 +0900

    javascriptライブラリ読み込み追加

--- index.php
+++ index.php
@@ -76,5 +76,8 @@ while($row = $stmt->fetch())
 ?>
 		    </table>
         </div>
+
+        <script src="js/jquery-1.11.1.js"></script>
+        <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
