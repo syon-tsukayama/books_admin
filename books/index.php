588a252ccbf3949e266a37cdda466949c05bcc50
tsukayama <tsukayama@syon.co.jp>
2014-06-02 14:20:04 +0900

    tableタグインデント修正

--- books/index.php
+++ books/index.php
@@ -301,20 +301,20 @@ if($is_search_option)
 // SQL実行
 $result = $stmt->execute();
 ?>
-    <div class="row">
-        <div class="col-md-12">
-    <table class="table table-striped table-hover">
-        <tr>
-            <th>ID</th>
-            <th>図書名</th>
-            <th>図書名カナ</th>
-            <th>著者名</th>
-            <th>著者名カナ</th>
-            <th>登録日時</th>
-            <th>更新日時</th>
-            <th>編集</th>
-            <th>削除</th>
-        </tr>
+            <div class="row">
+                <div class="col-md-12">
+                    <table class="table table-striped table-hover">
+                        <tr>
+                            <th>ID</th>
+                            <th>図書名</th>
+                            <th>図書名カナ</th>
+                            <th>著者名</th>
+                            <th>著者名カナ</th>
+                            <th>登録日時</th>
+                            <th>更新日時</th>
+                            <th>編集</th>
+                            <th>削除</th>
+                        </tr>
 <?php
 
 // SQL実行結果確認
@@ -325,30 +325,30 @@ if($result)
     {
         $book_id = $row['id'];
 ?>
-    <tr>
-        <td><?php echo $book_id; ?></td>
-        <td><?php echo $row['book_name']; ?></td>
-        <td><?php echo $row['book_kana']; ?></td>
-        <td><?php echo $row['author_name']; ?></td>
-        <td><?php echo $row['author_kana']; ?></td>
-        <td><?php echo $row['created']; ?></td>
-        <td><?php echo $row['updated']; ?></td>
-        <td>
-            <a href="edit_form.php?book_id=<?php echo $book_id; ?>" class="btn btn-default">
-                <i class="glyphicon glyphicon-edit"></i> 編集
-            </a>
-        </td>
-        <td>
-            <a href="delete.php?book_id=<?php echo $book_id; ?>" class="btn btn-default" onclick="if(confirm('削除しますよー？')){return true;} return false;">
-                <i class="glyphicon glyphicon-trash"></i> 削除
-            </a>
-        </td>
-    </tr>
+                        <tr>
+                            <td><?php echo $book_id; ?></td>
+                            <td><?php echo $row['book_name']; ?></td>
+                            <td><?php echo $row['book_kana']; ?></td>
+                            <td><?php echo $row['author_name']; ?></td>
+                            <td><?php echo $row['author_kana']; ?></td>
+                            <td><?php echo $row['created']; ?></td>
+                            <td><?php echo $row['updated']; ?></td>
+                            <td>
+                                <a href="edit_form.php?book_id=<?php echo $book_id; ?>" class="btn btn-default">
+                                    <span class="glyphicon glyphicon-edit"></span> 編集
+                                </a>
+                            </td>
+                            <td>
+                                <a href="delete.php?book_id=<?php echo $book_id; ?>" class="btn btn-default" onclick="if(confirm('削除しますよー？')){return true;} return false;">
+                                    <i class="glyphicon glyphicon-trash"></i> 削除
+                                </a>
+                            </td>
+                        </tr>
     <?php
     }
 }
 ?>
-                </table>
+                    </table>
                 </div>
             </div>
         </div>
