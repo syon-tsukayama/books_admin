f8ea1ee8826114740da3435a6b75d010dbcc83fc
tsukayama <tsukayama@syon.co.jp>
2014-06-07 12:33:52 +0900

    削除時の確認ダイアログ修正

--- users/index.php
+++ users/index.php
@@ -72,7 +72,7 @@ EOS;
                                 </a>
                             </td>
                             <td>
-                                <a href="delete.php?user_id=<?php echo $user_id; ?>" class="btn btn-default" onclick="if(confirm('削除しますよー？')){return true;} return false;">
+                                <a href="delete.php?user_id=<?php echo $user_id; ?>" class="btn btn-default" onclick="return confirm('削除しますよー？')">
                                     <span class="glyphicon glyphicon-trash"></span> 削除
                                 </a>
                             </td>
